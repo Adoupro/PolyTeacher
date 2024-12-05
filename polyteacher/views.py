630579360358c960534315e63e3862bd0cbe1bbc
mathysfruchard <mathys.fruchard@stfelixlasalle.fr>
2024-12-05 09:06:45 +0100

    code a jour

---
+++ polyteacher/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from translator.models import Translation
+from translator.serializers import TranslationSerializer
+
+# Create your views here.
+class AllTranslation(APIView):
+    def get(self, request):
+
+        result = Translation.objects.all()
+        serialized_data = TranslationSerializer(result, many=True)
+
+        return Response(data=serialized_data.data, status=status.HTTP_200_OK)
+
+class FrenchSpanishTranslationViewSet(APIView):
+
+    def get(self, request):
+        return Response(data={'result': "OK"}, status=None)
+    
+    def post(self, request):
+        return Response(data={}, status=None)
+    
+    def put(self, request, pk):
+        return Response(data={}, status=None)
+    
+    def delete(self, request, pk):
+        return Response(data={}, status=None)
+    
+class FrenchEnglishTranslationViewSet(APIView):
+
+    def get(self, request):
+        return Response(data={}, status=None)
+    
+    def post(self, request):
+        return Response(data={}, status=None)
+    
+    def put(self, request, pk):
+        return Response(data={}, status=None)
+    
+    def delete(self, request, pk):
+        return Response(data={}, status=None)
+
+def index(request):
+    return render(request, 'index.html', context={})
